343ff13f2222483992f2372cb80eb95e0498fca1
rajeevyadav <rajeevy899@gmail.com>
2016-10-17 01:40:36 +0530

    singleOrderItem Layout added

--- app/src/main/java/in/curience/hacktrec/Activities/SingleOrderItemActivity.java
+++ app/src/main/java/in/curience/hacktrec/Activities/SingleOrderItemActivity.java
@@ -4,28 +4,28 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-import in.curience.hacktrec.Models.MenuData;
 import in.curience.hacktrec.R;
 
 /**
  * Created by RAJEEV YADAV on 10/16/2016.
  */
-public class SingleOrderItemActivity extends AppCompatActivity{
-
-
+public class SingleOrderItemActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.single_order_item_activity);
+        Spinner quantity = (Spinner) findViewById(R.id.item_quantity);
+        getItemCount(quantity);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.item_quantity, android.R.layout.simple_spinner_dropdown_item);
+        quantity.setAdapter(adapter);
+        Button fab= (Button) findViewById(R.id.fab);
 
-        MenuData itemInfo = (MenuData) getIntent().getSerializableExtra("details");
+    }
 
-        Spinner quantity= (Spinner) findViewById(R.id.item_quantity);
-
-        //ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.item_quantity,android.R.layout.simple_spinner_dropdown_item);
-
-
+    int getItemCount(Spinner quantity) {
+        return (int) quantity.getSelectedItem();
     }
 }

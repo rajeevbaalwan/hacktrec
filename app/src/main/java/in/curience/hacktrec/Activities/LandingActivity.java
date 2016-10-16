25a8831b43f664bce9b6b63ec2addf98a4e6e327
rajeevyadav <rajeevy899@gmail.com>
2016-10-16 20:23:00 +0530

    custom row added

--- app/src/main/java/in/curience/hacktrec/Activities/LandingActivity.java
+++ app/src/main/java/in/curience/hacktrec/Activities/LandingActivity.java
@@ -2,6 +2,8 @@ package in.curience.hacktrec.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import in.curience.hacktrec.R;
 
@@ -10,6 +12,14 @@ public class LandingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_landing);
+
     }
+
+
+
+
+
 }

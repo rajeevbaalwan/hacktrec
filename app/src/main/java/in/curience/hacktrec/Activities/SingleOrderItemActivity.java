b1237f9ef10e5a9172654e605029022587fd8741
rajeevyadav <rajeevy899@gmail.com>
2016-10-16 23:59:25 +0530

    singleOrderItem Activity added

---
+++ app/src/main/java/in/curience/hacktrec/Activities/SingleOrderItemActivity.java
@@ -0,0 +1,25 @@
+package in.curience.hacktrec.Activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import in.curience.hacktrec.R;
+
+/**
+ * Created by RAJEEV YADAV on 10/16/2016.
+ */
+public class SingleOrderItemActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.single_order_item_activity);
+        Spinner quantity= (Spinner) findViewById(R.id.item_quantity);
+
+        //ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.item_quantity,android.R.layout.simple_spinner_dropdown_item);
+
+
+    }
+}
